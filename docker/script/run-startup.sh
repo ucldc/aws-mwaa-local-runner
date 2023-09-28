f770e5f5b2ed1a91bd34b4373b52e455d8bc65ee
Amy Wieliczka <amywieliczka@gmail.com>
2023-09-28 16:16:48 -0700

    mount docker.sock & set perms; required for DockerOperator usage :socks:

--- docker/script/run-startup.sh
+++ docker/script/run-startup.sh
@@ -2,3 +2,7 @@
 
 source "$AIRFLOW_HOME/startup/startup.sh"
 declare -p | grep -v '^declare \-[aAilnrtux]*r ' > stored_env
+
+# allows the airflow user on the airflow docker container to access 
+# the host's docker.sock. This is needed for the DockerOperator to work.
+sudo chmod 666 /var/run/docker.sock
